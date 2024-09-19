dd223a8a3058caaac95044d92e76d900612e3a81
Angad <angadkumar70676@gmail.com>
2024-09-19 17:44:15 +0530

    Added Environment config

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import router from "./routes/user.js";
 import {connectDB} from "./database/database.js"
+import { config } from "dotenv";
 const app = express();
-const port = 3000;
+
+//dot env config
+config({
+    path:'./database/config.env'
+})
 
 //database connection
 connectDB();
@@ -11,4 +16,4 @@ connectDB();
 app.use(express.json());
 app.use(router)
 
-app.listen(port, () => console.log(`listening on port https://localhost:${port}`))
+app.listen(process.env.PORT, () => console.log(`listening on port https://localhost:${process.env.PORT}`))
